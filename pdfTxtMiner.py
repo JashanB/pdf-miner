6f943381239f46f496a15f03a04badab1acfcafd
JashanB <jashan_brar50@hotmail.com>
2020-06-27 19:20:38 -0400

    integrating pdfminer3k

--- pdfTxtMiner.py
+++ pdfTxtMiner.py
@@ -1,25 +1,31 @@
-fp = open(path, 'rb')
-    from pdfminer.pdfparser import PDFParser, PDFDocument
-    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-    from pdfminer.converter import PDFPageAggregator
-    from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+# pull file
+fp = open('./2020-tax.pdf', 'rb')
 
-    parser = PDFParser(fp)
-    doc = PDFDocument()
-    parser.set_document(doc)
-    doc.set_parser(parser)
-    doc.initialize('')
-    rsrcmgr = PDFResourceManager()
-    laparams = LAParams()
-    laparams.char_margin = 1.0
-    laparams.word_margin = 1.0
-    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    extracted_text = ''
+password = ""
+extracted_text = ""
 
-    for page in doc.get_pages():
-        interpreter.process_page(page)
-        layout = device.get_result()
-        for lt_obj in layout:
-            if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-                extracted_text += lt_obj.get_text()
+# parser stores objects from pdf
+# document requests objects from pdf
+parser = PDFParser(fp)
+document = PDFDocument()
+parser.set_document(document)
+document.set_parser(parser)
+document.initialize('')
+rsrcmgr = PDFResourceManager()
+laparams = LAParams()
+laparams.char_margin = 1.0
+laparams.word_margin = 1.0
+device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+interpreter = PDFPageInterpreter(rsrcmgr, device)
+extracted_text = ''
+
+for page in document.get_pages():
+    interpreter.process_page(page)
+    layout = device.get_result()
+    for lt_obj in layout:
+        if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+            extracted_text += lt_obj.get_text()
